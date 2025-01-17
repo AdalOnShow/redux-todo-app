9cc2046e45a0a8a383c92422fe0a64017fff3733
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-17 22:20:18 +0600

    Add TodoApp component with local storage integration for todos

---
+++ src/Todo.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo, toggleTodo } from "./store/todoSlice";
+
+const TodoApp = () => {
+  const [text, setText] = useState("");
+  const [editId, setEditId] = useState(null);
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    if (savedTodos.length > 0) {
+      savedTodos.forEach((todo) => {
+        dispatch({
+          type: "todos/loadFromStorage",
+          payload: todo
+        })
+      })
+    }
+  }, [dispatch])
+
+}
+
+export default TodoApp;
