fe708ba3b4a5199041bc1abcc22c5c46c17450af
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-17 22:03:42 +0600

    Add todoSlice with reducers for adding, toggling, and removing todos

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,2 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 
+const todoSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload, completed: false });
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload)
+
+      id(todo) {
+        todo.completed = !todo.completed
+      }
+    },
+    removeTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload)
+    }
+  })
